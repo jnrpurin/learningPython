d6d0b815cb47c025c62c6ad8044d1a39e4e829b7
Ademir <t.ademir.belini@estapar.com.br>
2023-06-13 11:33:45 -0300

    Checking the api response

--- NewAppKivy/main.py
+++ NewAppKivy/main.py
@@ -5,22 +5,27 @@ import requests
 UI = Builder.load_file("screen.kv")
 
 
+def get_quotation(coin):
+    link = f"https://economia.awesomeapi.com.br/last/{coin}-BRL"
+    response = requests.get(link)
+
+    if response.status_code == 200:
+        quotation = response.json()
+        return quotation[f"{coin}BRL"]["bid"]
+    else:
+        print(f"API {link} is unavailable")
+        return 0
+
+
 class MyNewApp(App):
     def build(self):
         return UI
 
     def on_start(self):
-        self.root.ids["mDollar"].text = f"Dollar = R${self.get_values('USD')}"
-        self.root.ids["mEuro"].text = f"Euro = R${self.get_values('EUR')}"
-        self.root.ids["mBitc"].text = f"Bitcoin = R${self.get_values('BTC')}"
-        self.root.ids["mEthe"].text = f"Ethereum = R${self.get_values('ETH')}"
-
-    def get_values(self, coin):
-        link = f"https://economia.awesomeapi.com.br/last/{coin}-BRL"
-        req = requests.get(link)
-        dicreq = req.json()
-        return dicreq[f"{coin}BRL"]["bid"]
-        
+        self.root.ids["mDollar"].text = f"Dollar = R${get_quotation('USD')}"
+        self.root.ids["mEuro"].text = f"Euro = R${get_quotation('EUR')}"
+        self.root.ids["mBitc"].text = f"Bitcoin = R${get_quotation('BTC')}"
+        self.root.ids["mEthe"].text = f"Ethereum = R${get_quotation('ETH')}"
 
 
 MyNewApp().run()
